f6451bba14b7a0edba5f66b8ff8bd2d5b2433eb3
Garner <garnerj@stifel.com>
2018-01-12 09:54:35 -0600

    rename stuff in editor UI

--- src/App.js
+++ src/App.js
@@ -11,29 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    const headerJson = {
-      description: "Page Header",
-      type: 'div',
-      props: {
-        className: 'App-header'
-      },
-      children: [
-        {
-          description: "Header Title",
-          type: 'h1',
-          props: {
-            className: 'App-title'
-          },
-          children: 'This is your page header.'
-        }, {
-          description: "SubHeader Text",
-          type: 'p',
-          children: 'This is your subheader'
-        }
-      ]
-    };
-
-    const bodyJson = {
+    const templateJson = {
       description: "Page Body",
       type: 'div',
       props: {
@@ -41,91 +19,49 @@ class App extends Component {
       },
       children: [
         {
-          description: "Body Header",
+          description: "Header",
           type: 'h2',
           props: {
             className: ''
           },
-          children: 'This is your page body'
+          children: 'This is the header text'
         }, {
-          description: "Body Content",
+          description: "Content",
           type: 'p',
           children: 'This is your content.'
         }, {
-          description: "Link p tag",
+          description: "A p tag to hold a link",
           type: 'p',
           children: [
             {
-              description: "Link",
+              description: "A link",
               type: 'a',
               props: {
                 href: "http://www.stifel.com",
-                target:"_blank"
+                target: "_blank"
               },
               children: "stifel.com"
             }
           ]
         }, {
-          description: "Body image",
+          description: "An image",
           type: 'img',
           props: {
             src: "https://pbs.twimg.com/profile_images/907658468898861057/HLNNoMPm.jpg",
-            alt:"Stifel Logo",
-            height:"120"
+            alt: "Stifel Logo",
+            height: "120"
           }
         }
       ]
     };
 
-    const footerJson = {
-      description: "Footer",
-      type: 'div',
-      props: {
-        className: 'App-footer'
-      },
-      children: [
-        {
-          type: 'hr'
-        }, {
-          description: "Footer text",
-          type: 'p',
-          props: {
-            className: ''
-          },
-          children: 'This is your footer'
-        }, {
-          description: "Footer text",
-          type: 'p',
-          children: '&#169; Stifel, disclaimers and stuff'
-        }
-      ]
-    };
-
-    const components = [headerJson, bodyJson, footerJson]
-
-    const pageTemplateJson = {
-      description: 'page container',
-      props: {
-        className: 'container'
-      },
-      type: 'div',
-      children: [
-        ...components, {
-          description: 'page title',
-          props: {
-            title: "My Title"
-          },
-          type: Helmet
-        }
-      ]
-    };
-
     this.state = {
-      pageJson: pageTemplateJson
+      pageJson: templateJson
     };
 
     this.onJsonChange = (key, value, parent, data) => {
-      if(value === '') return;
+      if (value === '')
+        return;
       let pageJson = Object.assign({}, this.state.pageJson); //creating copy of object
       pageJson = data; //updating value
       this.setState({pageJson});
@@ -138,27 +74,24 @@ class App extends Component {
       <Grid>
         <Row>
           <Col xs={12} md={4} lg={4} lgPull={2}>
-            <h2>CMS Proof of Concept</h2>
-            <p>The page below is built dynamically from metadata stored in a JSON file. Clearly we would want a more user-friendly way of editing the metadata than this.
+            <h2>Proof of Concept</h2>
+            <p>The editor below is bound to a JSON object that describes the template component DOM, styling and content.
             </p>
           </Col>
           <Col xs={12} md={8} lg={8}>
-            <h3>Advantages</h3>
-            <ul>
-              <li>It's possible to modularize the json metadata into components for reuse.</li>
-              <li>Your editor will output one complete JSON file that represents a complete page - this means you've got a clear, compact way of moving a page through an approval process and into production without a lot of additional complexity.
-              </li>
-            </ul>
+            <h3>Rendered Component</h3>
+            <p>The JSON is parsed into javascript which is executed on the page and renders the HTML you see below.</p>
           </Col>
         </Row>
         <Row >
-          <Col xs={12} md={3} lg={2} lgPull={2}>
+          <Col xs={12} md={4} lg={3} lgPull={2}>
             <h3>Editor</h3>
-            <p>Change values below to update the page.</p>
+            <hr/>
             <JSONEditor data={this.state.pageJson} onChange={this.onJsonChange}/>
           </Col>
-          <Col xs={12} md={9} lg={10}>
-            <h3>Rendered Page</h3>
+          <Col xs={12} md={8} lg={9}>
+            <h3>Example CMS Component</h3>
+            <hr/>
             <PageBuilder json={this.state.pageJson}/>
           </Col>
         </Row>
--- src/components/JSONEditor.js
+++ src/components/JSONEditor.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {isArray, isObject, isNumber, isString, isBoolean, merge, cloneDeep} from 'lodash';
+import {
+  isArray,
+  isObject,
+  isNumber,
+  isString,
+  isBoolean,
+  merge,
+  cloneDeep
+} from 'lodash';
 import JSONViewer from './JSONViewer';
 import {CollapseIcon, isNodeCollapsed, toggleNodeCollapsed} from './CollapseIcon';
 
@@ -7,7 +15,7 @@ export default class JSONEditor extends React.Component {
 
   static defaultProps = {
     data: {}, //data to edit
-    marginLeftStep: 10, //indentation step for nested objects
+    marginLeftStep: 6, //indentation step for nested objects
     marginBottom: 3, //margin bottom of nodes
     collapsible: true, //whether nodes are collapsible or not
     //this prevents modifying the data you passed in however cloning is expensive especially for large objects
@@ -15,203 +23,250 @@ export default class JSONEditor extends React.Component {
     onChange: null, //data changed handler,
     view: "single", //dual, shows the editor and the json viewer side to side,
     collapsedNodes: {},
-    synchronizedCollapse: true //if in dual view when editor is collapsed, viewer is also collapsed 
+    synchronizedCollapse: true //if in dual view when editor is collapsed, viewer is also collapsed
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data: {'root' : props.cloneData ? cloneDeep(props.data) : props.data},
+      data: {
+        'root': props.cloneData
+          ? cloneDeep(props.data)
+          : props.data
+      },
       collapsedNodes: this.props.collapsedNodes
     };
   }
 
-  getCollapseIcon(marginLeft, prevKey){
+  getCollapseIcon(marginLeft, prevKey) {
     let {collapsedNodes} = this.state;
     let {collapsible, marginLeftStep} = this.props;
-    return (
-      <CollapseIcon 
-        collapsedNodes={collapsedNodes} 
-        marginLeft={marginLeft} 
-        collapsible={collapsible} 
-        prevKey={prevKey}
-        isNodeCollapsed={isNodeCollapsed.bind(this, marginLeft, prevKey, marginLeftStep)}
-        toggleNodeCollapsed={toggleNodeCollapsed.bind(this, marginLeft, prevKey, marginLeftStep)}
-      />
-    )
+    return (<CollapseIcon collapsedNodes={collapsedNodes} marginLeft={marginLeft} collapsible={collapsible} prevKey={prevKey} isNodeCollapsed={isNodeCollapsed.bind(this, marginLeft, prevKey, marginLeftStep)} toggleNodeCollapsed={toggleNodeCollapsed.bind(this, marginLeft, prevKey, marginLeftStep)}/>)
   }
 
-  dataChanged(key, parent, type, e){
+  dataChanged(key, parent, type, e) {
     let value = this.castToType(e.target.value, type);
     parent[key] = value;
     this.setState(this.state.data);
-    if(this.props.onChange) this.props.onChange(key, value, parent, this.state.data.root);
-  }
+    if (this.props.onChange)
+      this.props.onChange(key, value, parent, this.state.data.root);
+    }
 
-  castToType(value, type){
-    switch(type){
+  castToType(value, type) {
+    switch (type) {
       case "number":
         return Number(value);
       case "string":
         return String(value);
       case "boolean":
-        return value === "true" ? true : false;
+        return value === "true"
+          ? true
+          : false;
       default:
         return value;
     }
   }
 
-  recursiveParseData(prevKey, parent, elems, marginLeft){
-    let data = parent[prevKey]; 
+  recursiveParseData(prevKey, parent, elems, marginLeft) {
+    let data = parent[prevKey];
     let label = prevKey;
     let {marginLeftStep} = this.props;
-    if(isArray(parent)) {
+    if (isArray(parent)) {
       label += 1;
       label += "."
     }
-    if(isArray(data)){
-      if(marginLeft > 0){ //special case to avoid showing root
-        elems.push(
-          <ParentLabel 
-            value={label} 
-            marginLeft={marginLeft}
-            prevKey={prevKey}
-            getCollapseIcon={this.getCollapseIcon.bind(this)}
-          />
-        );
+    if (isArray(data)) {
+      if (marginLeft > 0) { //special case to avoid showing root
+        elems.push(<ParentLabel value={label} marginLeft={marginLeft} prevKey={prevKey} getCollapseIcon={this.getCollapseIcon.bind(this)} data={data}/>);
       }
 
-      if(isNodeCollapsed.call(this, marginLeft, prevKey, marginLeftStep)) return; //this node is collapsed
+      if (isNodeCollapsed.call(this, marginLeft, prevKey, marginLeftStep))
+        return; //this node is collapsed
 
-      for(let key = 0; key < data.length; key++){
+      for (let key = 0; key < data.length; key++) {
         this.recursiveParseData(key, data, elems, marginLeft + marginLeftStep);
       }
 
-    } else if(isObject(data)){
+    } else if (isObject(data)) {
 
-      if(marginLeft > 0){//special case to avoid showing root
-        elems.push(
-          <ParentLabel 
-            value={label} 
-            marginLeft={marginLeft}
-            prevKey={prevKey}
-            getCollapseIcon={this.getCollapseIcon.bind(this)}
-          />
-        );
+      if (marginLeft > 0) { //special case to avoid showing root
+        elems.push(<ParentLabel value={label} marginLeft={marginLeft} prevKey={prevKey} getCollapseIcon={this.getCollapseIcon.bind(this)} data={data}/>);
       }
 
-      if(isNodeCollapsed.call(this, marginLeft, prevKey, marginLeftStep)) return; //this node is collapsed
-      
+      if (isNodeCollapsed.call(this, marginLeft, prevKey, marginLeftStep))
+        return; //this node is collapsed
+
       Object.keys(data).forEach(key => {
         this.recursiveParseData(key, data, elems, marginLeft + marginLeftStep);
       });
 
-    } else if(isNumber(data)){
-      elems.push(
-        <Input
-          marginLeft={marginLeft} 
-          marginBottom={this.props.marginBottom}
-          label={label} 
-          type="number"
-          onChange={this.dataChanged.bind(this, prevKey, parent, 'number')}
-          value={data}/>
-      );
-    } else if(isString(data)) {
-      elems.push(
-        <Input 
-          marginLeft={marginLeft} 
-          marginBottom={this.props.marginBottom}
-          label={label} 
-          type="text"
-          onChange={this.dataChanged.bind(this, prevKey, parent, 'text')}
-          value={data}/>
-      );
-    } else if(isBoolean(data)){
-      elems.push(
-        <Boolean 
-          marginLeft={marginLeft} 
-          marginBottom={this.props.marginBottom}
-          onChange={this.dataChanged.bind(this, prevKey, parent, 'boolean')}
-          label={label} 
-          value={data}/>
-      );
+    } else if (isNumber(data)) {
+      elems.push(<Input marginLeft={marginLeft} marginBottom={this.props.marginBottom} label={label} type="number" onChange={this.dataChanged.bind(this, prevKey, parent, 'number')} value={data}/>);
+    } else if (isString(data)) {
+      elems.push(<Input marginLeft={marginLeft} marginBottom={this.props.marginBottom} label={label} type="text" onChange={this.dataChanged.bind(this, prevKey, parent, 'text')} value={data}/>);
+    } else if (isBoolean(data)) {
+      elems.push(<Boolean marginLeft={marginLeft} marginBottom={this.props.marginBottom} onChange={this.dataChanged.bind(this, prevKey, parent, 'boolean')} label={label} value={data}/>);
     }
   }
 
-  render(){
+  render() {
     let elems = [];
+    elems.push(<Header/>)
     let {view, collapsible, synchronizedCollapse} = this.props;
     let {data, collapsedNodes} = this.state;
     this.recursiveParseData('root', data, elems, 0);
-    if(view === "single"){
+    if (view === "single") {
       return <div style={styles.root}>{elems}</div>
-    } else if(view === "dual"){
-      return (
-        <div style={styles.dualView}>
-          <div style={styles.jsonEditor}>{elems}</div>
-          <div style={styles.jsonViewer}>
-            <JSONViewer 
-              data={data.root} 
-              collapsible={collapsible} 
-              collapsedNodes={synchronizedCollapse ? collapsedNodes : {}}/>
-          </div>
+    } else if (view === "dual") {
+      return (<div style={styles.dualView}>
+        <div style={styles.jsonEditor}>{elems}</div>
+        <div style={styles.jsonViewer}>
+          <JSONViewer data={data.root} collapsible={collapsible} collapsedNodes={synchronizedCollapse
+              ? collapsedNodes
+              : {}}/>
         </div>
-      )
+      </div>)
     }
   }
 }
 
+const Header = (props) => {
+  return (<h4>Template Container</h4>)
+}
 
 const Input = (props) => {
-  let {marginLeft, marginBottom, label, value, type, onChange} = props;
-  let style = merge({marginLeft, marginBottom}, styles.row);
-  return (
-    <div style={style}>
-      <Label 
-        value={label} 
-        marginLeft={0}/>
-      <div style={styles.value}>
-        <input style={styles.input} type={type} value={value} onChange={onChange}/>
-      </div>
+  let {
+    marginLeft,
+    marginBottom,
+    label,
+    value,
+    type,
+    onChange
+  } = props;
+  let style = merge({
+    marginLeft,
+    marginBottom
+  }, styles.row);
+  let isHidden = false;
+  let inputComp = <input style={styles.input} type={type} value={value} onChange={onChange}/>
+
+  switch (label) {
+    case 'description':
+      isHidden = true;
+      // style = merge({
+      //   color: "#000000",
+      //   fontWeight: 'bold'
+      // }, style);
+      break;
+    case 'children':
+      inputComp = <textarea style={styles.input} onChange={onChange}>{value}</textarea>
+      break;
+  }
+
+  return (<div style={style}>
+    <Label value={label} marginLeft={0}/>
+    <div style={styles.value}>
+      {
+        (isHidden)
+          ? ''
+          : inputComp
+      }
     </div>
-  )
+  </div>)
 }
 
-const Boolean = (props) => {  //Boolean is reserved in javascript, rename to _Boolean?
-  let {marginLeft, marginBottom, label, value, onChange} = props;
-  let style = merge({marginLeft, marginBottom}, styles.row);
-  return (
-    <div style={style}>
-      <Label 
-        value={label} 
-        marginLeft={0}/>
-      <div style={styles.value}>
-        <select style={styles.select} value={value} onChange={onChange}>
-          <option value="true">True</option>
-          <option value="false">False</option>
-        </select>
-      </div>
-    </div>
-  );
-}
+class Label extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const Label = (props) => {
-  let {marginLeft, value} = props;
-  let style = merge({marginLeft}, styles.label);
-  return (
-    <div style={style}>{value}</div>
-  );
+  render() {
+    let {marginLeft, value} = this.props;
+    let style = merge({
+      marginLeft
+    }, styles.label);
+
+    let friendlylabel = '';
+
+    switch (value) {
+      case 'description':
+        friendlylabel = '';
+        style = {
+          color: "#c00",
+          marginLeft: 0,
+          marginTop: 0
+        }
+        break;
+      case 'type':
+        friendlylabel = 'HTML Tag:'
+        break;
+      case 'props':
+        friendlylabel = 'attributes'
+        break;
+      case 'children':
+        friendlylabel = 'text:'
+        break;
+      case 'className':
+        friendlylabel = 'CSS Classes:'
+        break;
+      default:
+        friendlylabel = value
+    }
+
+    return (<div style={style}>{friendlylabel}</div>);
+  }
 }
 
 const ParentLabel = (props) => {
-  let {marginLeft, value, prevKey, getCollapseIcon} = props;
-  let style = merge({marginLeft: marginLeft, display: "flex"}, styles.label);
-  return (
-    <div style={style}>
-      <div>{value}</div>
-      <div style={{marginLeft: 5}}>{getCollapseIcon(marginLeft, prevKey)}</div>
+  let {
+    marginLeft,
+    value,
+    prevKey,
+    getCollapseIcon,
+    data
+  } = props;
+  let style = merge({
+    marginLeft: marginLeft,
+    display: "flex",
+    fontWeight:'bold'
+  }, styles.label);
+
+  let friendlylabel = '';
+
+  switch (value) {
+    case 'props':
+      friendlylabel = 'attributes:'
+      break;
+    case 'children':
+      friendlylabel = 'elements:'
+      break;
+    default:
+      friendlylabel = data['description']
+  }
+
+  return (<div style={style}>
+    <div>{friendlylabel}</div>
+    <div style={{
+        marginLeft: 5
+      }}>{getCollapseIcon(marginLeft, prevKey)}</div>
+  </div>);
+}
+
+const Boolean = (props) => { //Boolean is reserved in javascript, rename to _Boolean?
+  let {marginLeft, marginBottom, label, value, onChange} = props;
+  let style = merge({
+    marginLeft,
+    marginBottom
+  }, styles.row);
+  return (<div style={style}>
+    <Label value={label} marginLeft={0}/>
+    <div style={styles.value}>
+      <select style={styles.select} value={value} onChange={onChange}>
+        <option value="true">True</option>
+        <option value="false">False</option>
+      </select>
     </div>
-  );
+  </div>);
 }
 
 const styles = {
@@ -230,18 +285,18 @@ const styles = {
     margin: 10
   },
   label: {
-    color: "#c00",
+    color: "#000000",
     marginTop: 4
   },
   value: {
-   marginLeft: 10
+    marginLeft: 10
   },
   row: {
-    display: "flex",
+    display: "flex"
   },
   root: {
     fontSize: 12,
-    fontFamily: "monospace"
+    fontFamily: "arial"
   },
   withChildrenLabel: {
     color: "#a52a2a"
